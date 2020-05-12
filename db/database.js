caed62dc61e28cb00ae86065d907c64f677f6344
Dennis <dennisweller@gmx.net>
2020-05-12 14:18:52 +0200

    Add student schema

--- db/database.js
+++ db/database.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const mdbString = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?retryWrites=true&w=majority'; 
 
-mongoose.connect('mongodb://'+process.env.DB_HOST, {
+console.log(mdbString);
+
+mongoose.connect(mdbString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+}, err => {
+    if (err) console.error('Could not connect to the database');
 });
 
 module.exports = mongoose;
--- routes/student.js
+++ routes/student.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('../db/database');
+const student = require('../schema/student');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Student route');
+router.get('/', cors(), (req, res) => {
+    db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => console.log('Student route'));
 });
 
 module.exports = router;
